53c4268ad07b09b6c790a561adae6daff77493e7
PapsonBoyRaphael <papsondevs@gmail.com>
2025-05-18 05:31:35 +0100

    Synch services

--- file-service/src/config/initDb.js
+++ file-service/src/config/initDb.js
@@ -1,12 +1,12 @@
-const { mongoose, File } = require("./db");
+// const { mongoose, File } = require("./db");
 
-const initFileDb = async () => {
-  try {
-    await File.deleteMany({});
-    console.log("✅ File_DB initialisée (aucun fichier de test par défaut)");
-  } catch (error) {
-    console.error("❌ Erreur lors de l'init File_DB:", error);
-  }
-};
+// const initFileDb = async () => {
+//   try {
+//     await File.deleteMany({});
+//     console.log("✅ File_DB initialisée (aucun fichier de test par défaut)");
+//   } catch (error) {
+//     console.error("❌ Erreur lors de l'init File_DB:", error);
+//   }
+// };
 
-module.exports = initFileDb;
+// module.exports = initFileDb;
